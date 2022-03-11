196edd6a2e945ebf970f3b9923ad6a5c8d3e2e63
a19336136 <aaaksenova@gmail.com>
2022-03-11 17:59:39 +0300

    add readme

--- gram_profiling/full_profiling_pipeline.sh
+++ gram_profiling/full_profiling_pipeline.sh
@@ -5,7 +5,7 @@
 # of the target words into JSON files
 
 TARGET=${1}  # List of target words, one per line
-CONLL0=${2}  # Directory with conllu.gz files
+CONLL_DIR=${2}  # Directory with conllu.gz files
 
 OUTJSONS=output/jsons
 mkdir -p ${OUTJSONS}
@@ -14,6 +14,6 @@ OUTSEPARATE=output/tsv
 mkdir -p ${OUTSEPARATE}
 
 echo "Extracting grammatical profiles..."
-python3 collect_ling_stats_child.py -i ${CONLL0} -t ${TARGET} -o ${OUTJSONS}/corpus0 #&
+python3 collect_ling_stats_child.py -i ${CONLL_DIR} -t ${TARGET} -o ${OUTJSONS}/corpus #&
 echo "Done extracting grammatical profiles"
 
